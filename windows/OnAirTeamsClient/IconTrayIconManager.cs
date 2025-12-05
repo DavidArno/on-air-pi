a0bcc3646e90b7e7bd6ca9899bb7890eed5672fa
David Arno <david@davidarno.org>
2025-12-05 09:23:45 +0000

    Looks like some simple C# language improvement changes

--- windows/OnAirTeamsClient/IconTrayIconManager.cs
+++ windows/OnAirTeamsClient/IconTrayIconManager.cs
@@ -5,154 +5,172 @@ using System.Drawing;
 using System.Windows.Forms;
 using static OnAirTeamsClient.IStatusNotifier.Statuses;
 
-namespace OnAirTeamsClient
+namespace OnAirTeamsClient;
+
+internal sealed class IconTrayIconManager : ApplicationContext, IStatusNotifier
 {
+    private const string AutoRunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+    private const string HkcuAutoRunKey = @"HKEY_CURRENT_USER\" + AutoRunKey;
+    private const string HkcuPersonalizeKey = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
 
-    internal sealed class IconTrayIconManager : ApplicationContext, IStatusNotifier
-    {
-        private const string AutoRunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
-        private const string HkcuAutoRunKey = @"HKEY_CURRENT_USER\" + AutoRunKey;
-        private const string HkcuPersonalizeKey = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+    private const string AppName = "OnAirTeamsClient";
 
-        private const string AppName = "OnAirTeamsClient";
+    private enum IconState { Disconnected, Connected, MicrophoneOn, WebcamOn }
 
-        private enum IconState { Disconnected, Connected, MicrophoneOn, WebcamOn }
-
-        private static readonly Icon[] DarkModeIcons = {
+    private static readonly Icon[] DarkModeIcons = {
             Resources.DisconnectedDark,
             Resources.ConnectedDark,
             Resources.MicrophoneOnDark,
             Resources.CameraOnDark
         };
 
-        private static readonly Icon[] LightModeIcons = {
+    private static readonly Icon[] LightModeIcons = {
             Resources.DisconnectedLight,
             Resources.ConnectedLight,
             Resources.MicrophoneOnLight,
             Resources.CameraOnLight
         };
 
-        private readonly NotifyIcon _trayIcon;
-        private readonly Icon[] _windowsModeIconSet;
+    private readonly NotifyIcon _trayIcon;
+    private readonly Icon[] _windowsModeIconSet;
 
-        private bool _serverConnected;
-        private bool _microphoneOn;
-        private bool _webcamOn;
+    private bool _serverConnected;
+    private bool _microphoneOn;
+    private bool _webcamOn;
 
-        private IconState _iconState;
-        private IconState _lastIconState;
+    private IconState _iconState;
+    private IconState _lastIconState;
 
+    public IconTrayIconManager()
+    {
+        var lightThemeValue = (int)Registry.GetValue(HkcuPersonalizeKey, "SystemUsesLightTheme", 0);
+        _windowsModeIconSet = lightThemeValue == 0 ? DarkModeIcons : LightModeIcons;
 
-        public IconTrayIconManager()
+        _trayIcon = new NotifyIcon()
         {
-            var lightThemeValue = (int)Registry.GetValue(HkcuPersonalizeKey, "SystemUsesLightTheme", 0);
-            _windowsModeIconSet = lightThemeValue == 0 ? DarkModeIcons : LightModeIcons;
-            
-            _trayIcon = new NotifyIcon() {
-                Icon = Resources.DisconnectedDark,
-                ContextMenu = GenerateContextMenu(),
-                Visible = true
-            };
+            Icon = Resources.DisconnectedDark,
+            ContextMenuStrip = GenerateContextMenu(),
+            Visible = true
+        };
 
-            new Timer {
-                Interval = 500,
-                Enabled = true
-            }.Tick += AnimationTimerTick;
-
-        }
-
-        private ContextMenu GenerateContextMenu()
-            => new ContextMenu(
-                new[] {
-                    new MenuItem("Exit", Exit),
-                    new MenuItem("-"),
-                    new MenuItem("Microphone Off", MicrophoneOff) { Enabled = _iconState != IconState.Disconnected },
-                    new MenuItem("Microphone On", MicrophoneOn) { Enabled = _iconState != IconState.Disconnected },
-                    new MenuItem("-"),
-                    new MenuItem("Webcam Off", WebcamOff) { Enabled = _iconState != IconState.Disconnected },
-                    new MenuItem("Webcam On", WebcamOn) { Enabled = _iconState != IconState.Disconnected },
-                    new MenuItem("-"),
-                    AppIsSetToAutoRun()
-                        ? new MenuItem("Stop auto-running on Windows start", StopAppAutoRunning)
-                        : new MenuItem("Automatically run when Windows starts", SetAppToAutoRun)
-                });
-
-        public Action SetMicrophoneOn { get; set; }
-        public Action SetMicrophoneOff { get; set; }
-
-        public Action SetWebcamOn { get; set; }
-        public Action SetWebcamOff { get; set; }
-
-        public void SetServerStatus(IStatusNotifier.Statuses status)
+        new Timer
         {
-            _serverConnected = status == On;
-            UpdateIconState();
-        }
+            Interval = 500,
+            Enabled = true
+        }.Tick += AnimationTimerTick;
 
-        public void SetMicrophoneStatus(IStatusNotifier.Statuses status)
-        {
-            _microphoneOn = status == On;
-            UpdateIconState();
-        }
-
-        public void SetWebcamStatus(IStatusNotifier.Statuses status)
-        {
-            _webcamOn = status == On;
-            UpdateIconState();
-        }
-
-        private static bool AppIsSetToAutoRun() => Registry.GetValue(HkcuAutoRunKey, AppName, null) != null;
-
-        private void SetAppToAutoRun(object sender, EventArgs e)
-        {
-            using var key = Registry.CurrentUser.OpenSubKey(AutoRunKey, true);
-            key?.SetValue(AppName, Application.ExecutablePath);
-            _trayIcon.ContextMenu = GenerateContextMenu();
-        }
-
-        private void StopAppAutoRunning(object sender, EventArgs e)
-        {
-            using var key = Registry.CurrentUser.OpenSubKey(AutoRunKey, true);
-            key?.DeleteValue(AppName);
-            _trayIcon.ContextMenu = GenerateContextMenu();
-        }
-
-        private void Exit(object sender, EventArgs e)
-        {
-            _trayIcon.Visible = false;
-            Application.Exit();
-        }
-
-        private void MicrophoneOff(object sender, EventArgs e) => SetMicrophoneOff?.Invoke();
-
-        private void MicrophoneOn(object sender, EventArgs e) => SetMicrophoneOn?.Invoke();
-
-        private void WebcamOff(object sender, EventArgs e) => SetWebcamOff?.Invoke();
-
-        private void WebcamOn(object sender, EventArgs e) => SetWebcamOn?.Invoke();
-
-        private void AnimationTimerTick(object sender, EventArgs e)
-        {
-            if (_iconState == _lastIconState) return;
-
-            _lastIconState = _iconState;
-
-            _trayIcon.Icon = _iconState switch {
-                IconState.Disconnected => _windowsModeIconSet[0],
-                IconState.Connected => _windowsModeIconSet[1],
-                IconState.MicrophoneOn => _windowsModeIconSet[2],
-                _ => _windowsModeIconSet[3]
-            };
-
-            _trayIcon.ContextMenu = GenerateContextMenu();
-        }
-
-        private void UpdateIconState() 
-            => _iconState = (_webcamOn, _microphoneOn, _serverConnected) switch {
-                (_, _, false) => IconState.Disconnected,
-                (true, _, _) => IconState.WebcamOn,
-                (_, true, _) => IconState.MicrophoneOn,
-                _ => IconState.Connected
-            };
     }
+
+    private ContextMenuStrip GenerateContextMenu()
+    {
+        var menu = new ContextMenuStrip();
+        _ = menu.Items.Add(new ToolStripMenuItem("Exit", null, Exit));
+        _ = menu.Items.Add(new ToolStripSeparator());
+        _ = menu.Items.Add(
+            new ToolStripMenuItem("Microphone Off", null, MicrophoneOff) {
+                Enabled = _iconState != IconState.Disconnected
+            });
+    
+        _ = menu.Items.Add(
+            new ToolStripMenuItem("Microphone On", null, MicrophoneOn) {
+                Enabled = _iconState != IconState.Disconnected
+            });
+
+        _ = menu.Items.Add(new ToolStripSeparator());
+        _ = menu.Items.Add(
+            new ToolStripMenuItem("Webcam Off", null, WebcamOff) {
+                Enabled = _iconState != IconState.Disconnected
+            });
+        
+        _ = menu.Items.Add(
+            new ToolStripMenuItem("Webcam On", null, WebcamOn) {
+                Enabled = _iconState != IconState.Disconnected
+            });
+
+        _ = menu.Items.Add(new ToolStripSeparator());
+        _ = menu.Items.Add(AppIsSetToAutoRun()
+            ? new ToolStripMenuItem("Stop auto-running on Windows start", null, StopAppAutoRunning)
+            : new ToolStripMenuItem("Automatically run when Windows starts", null, SetAppToAutoRun));
+
+        return menu;
+    }
+
+    public Action SetMicrophoneOn { get; set; }
+    public Action SetMicrophoneOff { get; set; }
+
+    public Action SetWebcamOn { get; set; }
+    public Action SetWebcamOff { get; set; }
+
+    public void SetServerStatus(IStatusNotifier.Statuses status)
+    {
+        _serverConnected = status == On;
+        UpdateIconState();
+    }
+
+    public void SetMicrophoneStatus(IStatusNotifier.Statuses status)
+    {
+        _microphoneOn = status == On;
+        UpdateIconState();
+    }
+
+    public void SetWebcamStatus(IStatusNotifier.Statuses status)
+    {
+        _webcamOn = status == On;
+        UpdateIconState();
+    }
+
+    private static bool AppIsSetToAutoRun() => Registry.GetValue(HkcuAutoRunKey, AppName, null) != null;
+
+    private void SetAppToAutoRun(object sender, EventArgs e)
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(AutoRunKey, true);
+        key?.SetValue(AppName, Application.ExecutablePath);
+        _trayIcon.ContextMenuStrip = GenerateContextMenu();
+    }
+
+    private void StopAppAutoRunning(object sender, EventArgs e)
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(AutoRunKey, true);
+        key?.DeleteValue(AppName);
+        _trayIcon.ContextMenuStrip = GenerateContextMenu();
+    }
+
+    private void Exit(object sender, EventArgs e)
+    {
+        _trayIcon.Visible = false;
+        Application.Exit();
+    }
+
+    private void MicrophoneOff(object sender, EventArgs e) => SetMicrophoneOff?.Invoke();
+
+    private void MicrophoneOn(object sender, EventArgs e) => SetMicrophoneOn?.Invoke();
+
+    private void WebcamOff(object sender, EventArgs e) => SetWebcamOff?.Invoke();
+
+    private void WebcamOn(object sender, EventArgs e) => SetWebcamOn?.Invoke();
+
+    private void AnimationTimerTick(object sender, EventArgs e)
+    {
+        if (_iconState == _lastIconState) return;
+
+        _lastIconState = _iconState;
+
+        _trayIcon.Icon = _iconState switch {
+            IconState.Disconnected => _windowsModeIconSet[0],
+            IconState.Connected => _windowsModeIconSet[1],
+            IconState.MicrophoneOn => _windowsModeIconSet[2],
+            _ => _windowsModeIconSet[3]
+        };
+
+        _trayIcon.ContextMenuStrip = GenerateContextMenu();
+    }
+
+    private void UpdateIconState()
+        => _iconState = (_webcamOn, _microphoneOn, _serverConnected) switch {
+            (_, _, false) => IconState.Disconnected,
+            (true, _, _) => IconState.WebcamOn,
+            (_, true, _) => IconState.MicrophoneOn,
+            _ => IconState.Connected
+        };
 }
+
--- windows/OnAirTeamsClient/Program.cs
+++ windows/OnAirTeamsClient/Program.cs
@@ -1,64 +1,56 @@
-﻿using System;
+﻿using OnAirTeamsClient;
+using System;
 using System.Windows.Forms;
 
-namespace OnAirTeamsClient
+SingleGlobalInstance.Enforce(Run, ShowAlreadyRunningMessage);
+
+static void Run()
 {
-    internal static class Program
-    {
-        [STAThread]
-        private static void Main() => SingleGlobalInstance.Enforce(Run, ShowAlreadyRunningMessage);
+    const string webCam = "webcam";
+    const string microphone = "microphone";
+    const string serverHostName = "on-air-pi";
+    const int serverPort = 65432;
 
-        private static void Run()
-        {
-            const string webCam = "webcam";
-            const string microphone = "microphone";
-            const string serverHostName = "on-air-pi";
-            const int serverPort = 65432;
+    var username = Environment.UserName;
 
-            var username = Environment.UserName;
+    var webcamKey = BuildRegistryKeyString(webCam, username);
+    var microphoneKey = BuildRegistryKeyString(microphone, username);
 
-            var webcamKey = BuildRegistryKeyString(webCam, username);
-            var microphoneKey = BuildRegistryKeyString(microphone, username);
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+    var iconManager = new IconTrayIconManager();
 
-            var iconManager = new IconTrayIconManager();
+    var messageSender = new MessageSender(serverHostName, serverPort, iconManager.SetServerStatus);
 
-            var messageSender = new MessageSender(serverHostName, serverPort, iconManager.SetServerStatus);
+    using var webcamClientControl =
+        new DeviceStatusNotifier(webCam, messageSender, webcamKey, iconManager.SetWebcamStatus);
 
-            using var webcamClientControl = 
-                new DeviceStatusNotifier(webCam, messageSender, webcamKey, iconManager.SetWebcamStatus); 
+    using var microphoneClientControl =
+        new DeviceStatusNotifier(microphone, messageSender, microphoneKey, iconManager.SetMicrophoneStatus);
 
-            using var microphoneClientControl =
-                new DeviceStatusNotifier(microphone, messageSender, microphoneKey, iconManager.SetMicrophoneStatus);
+    using var webcamMonitor = new RegistryMonitor(webcamKey, webcamClientControl);
+    using var microphoneMonitor = new RegistryMonitor(microphoneKey, microphoneClientControl);
 
-            using var webcamMonitor = new RegistryMonitor(webcamKey, webcamClientControl);
-            using var microphoneMonitor = new RegistryMonitor(microphoneKey, microphoneClientControl);
+    iconManager.SetWebcamOff = webcamClientControl.OverrideDeviceStateToOff;
+    iconManager.SetWebcamOn = webcamClientControl.OverrideDeviceStateToOn;
 
-            iconManager.SetWebcamOff = webcamClientControl.OverrideDeviceStateToOff;
-            iconManager.SetWebcamOn= webcamClientControl.OverrideDeviceStateToOn;
+    iconManager.SetMicrophoneOff = microphoneClientControl.OverrideDeviceStateToOff;
+    iconManager.SetMicrophoneOn = microphoneClientControl.OverrideDeviceStateToOn;
 
-            iconManager.SetMicrophoneOff = microphoneClientControl.OverrideDeviceStateToOff;
-            iconManager.SetMicrophoneOn = microphoneClientControl.OverrideDeviceStateToOn;
-
-            microphoneClientControl.OnDeviceChanged();
-            webcamClientControl.OnDeviceChanged();
-
-            
-            Application.Run(iconManager);
-            
-        }
-
-        private static void ShowAlreadyRunningMessage()
-            => MessageBox.Show(
-                "Only one instance of the client can be run at a time.",
-                "OnAirTeamsClient is already running",
-                MessageBoxButtons.OK);
+    microphoneClientControl.OnDeviceChanged();
+    webcamClientControl.OnDeviceChanged();
 
 
-        private static string BuildRegistryKeyString(string device, string user)
-            => $@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\{device}\NonPackaged" +
-               $@"\C:#Users#{user}#AppData#Local#Microsoft#Teams#current#Teams.exe";
-    }
+    Application.Run(iconManager);
 }
+
+static void ShowAlreadyRunningMessage() => MessageBox.Show(
+    "Only one instance of the client can be run at a time.",
+    "OnAirTeamsClient is already running",
+    MessageBoxButtons.OK);
+
+
+static string BuildRegistryKeyString(string device, string user)
+    => $@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\{device}\NonPackaged" +
+       $@"\C:#Users#{user}#AppData#Local#Microsoft#Teams#current#Teams.exe";
